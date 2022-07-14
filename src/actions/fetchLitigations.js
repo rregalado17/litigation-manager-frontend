d75b58bc38948208e45fdfed0a5fc63e340f6446
rregalado17 <rregalado17@gmail.com>
2022-07-13 23:41:05 -0500

    reduz update

--- src/actions/fetchLitigations.js
+++ src/actions/fetchLitigations.js
@@ -1,11 +1,11 @@
-export function fetchLitigations() {
+// export function fetchLitigations() {
     
-    return (dispatch) => {
-        fetch('http://localhost:3000/api/v1/litigations')
-        .then(res => res.json())
-        .then(litigations => dispatch({
-            type: 'FETCH_LITIGATIONS', 
-            payload: litigations
-        }))
-    }
-}
+//     return (dispatch) => {
+//         fetch('http://localhost:3000/api/v1/litigations')
+//         .then(res => res.json())
+//         .then(litigations => dispatch({
+//             type: 'FETCH_LITIGATIONS', 
+//             payload: litigations
+//         }))
+//     }
+// }
